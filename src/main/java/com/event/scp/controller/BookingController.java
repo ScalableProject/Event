7e82cf28a56a15910986627dddc43be36960610e
Abubakar <abubakargill538@gmail.com>
2022-04-07 16:43:45 +0100

    evenr2

--- src/main/java/com/event/scp/controller/BookingController.java
+++ src/main/java/com/event/scp/controller/BookingController.java
@@ -1,6 +1,8 @@
 package com.event.scp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.event.scp.entity.Booking;
 import com.event.scp.exception.ResourceNotFoundException;
@@ -9,25 +11,41 @@ import com.event.scp.repository.BookingRepository;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/bookings")
+//@CrossOrigin(origins="*/")
 public class BookingController {
 
+	
+	@Bean
+    public WebMvcConfigurer getCorsConfiguration(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+	
 	@Autowired
 	private BookingRepository bookingRepositery;
 	
 	//Getting all bookings
-	@GetMapping
+	@GetMapping("/all")
 	public List <Booking> getAllBookings(){
 		
 		return this.bookingRepositery.findAll();
 	}
 	
 	//getting by id
-	@GetMapping("/{id}")
+	@GetMapping("/filter/{id}")
 	public Booking getBookingById(@PathVariable (value = "id") long bookingId) {
 		return this.bookingRepositery.findById(bookingId).orElseThrow(() -> new ResourceNotFoundException("Booking not found with id :" + bookingId));
 	}
@@ -49,8 +67,14 @@ public class BookingController {
 		existing.setfName(booking.getfName());
 		existing.setlName(booking.getlName());
 		existing.setAddress(booking.getAddress());
+		existing.setCity(booking.getCity());
+		existing.setEircode(booking.getEircode());
 		existing.setEmail(booking.getEmail());
 		existing.setNumber(booking.getNumber());
+		existing.setBooking_from(booking.getBooking_from());
+		existing.setBooking_till(booking.getBooking_till());
+		existing.setNo_people(booking.getNo_people());
+		existing.setRequest(booking.getRequest());
 		
 		return this.bookingRepositery.save(existing);
 		
